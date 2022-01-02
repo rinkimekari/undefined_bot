f7e44c46ff6c2252d38b6c35003607aa1b9f68ff
Matt Ehrnschwender <matthewe2020@gmail.com>
2022-01-01 22:23:32 -0500

    Continued joke

--- src/commands/joke.rs
+++ src/commands/joke.rs
@@ -1,8 +1,35 @@
+use serde::Deserialize;
 use serenity::client::Context;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::channel::Message;
 
+#[allow(unused)]
 const JOKE_URL: &'static str = "https://v2.jokeapi.dev/joke/Any";
+
+#[derive(Deserialize)]
+struct JokeFlags {
+    nsfw: bool,
+    religious: bool,
+    political: bool,
+    racist: bool,
+    sexist: bool,
+    explicit: bool,
+}
+
+#[derive(Deserialize)]
+struct JokeData {
+    error: bool,
+    category: String,
+    #[serde(rename = "type")]
+    joke_type: String,
+    setup: Option<String>,
+    delivery: Option<String>,
+    joke: Option<String>,
+    flags: JokeFlags,
+    id: u32,
+    safe: bool,
+    lang: String,
+}
 
 #[command]
 pub async fn joke(ctx: &Context, msg: &Message) -> CommandResult {
