7d2a12f8887002265624f079898928afc9cd6759
rinkimekari <micahwasmuth@gmail.com>
2022-01-01 22:06:07 -0500

    gnu mod

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,8 +1,11 @@
 use serenity::framework::standard::macros::group;
 
 mod ping;
+mod gnu;
+
 use ping::PING_COMMAND;
+use gnu::GNU_COMMAND;
 
 #[group]
-#[commands(ping)]
+#[commands(ping, gnu)]
 pub struct General;
