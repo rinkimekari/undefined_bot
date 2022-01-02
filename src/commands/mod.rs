fe2780abb59cce19a88c4f0d1b4d15c0aaa07b81
rinkimekari <micahwasmuth@gmail.com>
2022-01-02 00:37:50 -0500

    linux

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -5,13 +5,15 @@ mod ping;
 mod gnu;
 mod leet;
 mod help;
+mod linux;
 
 use joke::JOKE_COMMAND;
 use ping::PING_COMMAND;
 use gnu::GNU_COMMAND;
 use leet::LEET_COMMAND;
 use help::HELP_COMMAND;
+use linux::LINUX_COMMAND;
 
 #[group]
-#[commands(ping, joke, gnu, leet, help)]
+#[commands(ping, joke, gnu, leet, help, linux)]
 pub struct General;
