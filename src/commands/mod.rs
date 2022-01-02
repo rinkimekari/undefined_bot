c68649c24e83e01869eacc11a356bc7ec2dfe5b5
rinkimekari <micahwasmuth@gmail.com>
2022-01-01 22:52:31 -0500

    begin leet

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,11 +3,13 @@ use serenity::framework::standard::macros::group;
 mod joke;
 mod ping;
 mod gnu;
+mod leet;
 
 use joke::JOKE_COMMAND;
 use ping::PING_COMMAND;
 use gnu::GNU_COMMAND;
+use leet::LEET_COMMAND;
 
 #[group]
-#[commands(ping, joke, gnu)]
+#[commands(ping, joke, gnu, leet)]
 pub struct General;
