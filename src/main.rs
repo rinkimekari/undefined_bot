e05b6e5bed3310de35746d43c0a572c0c7c0dca5
Matt Ehrnschwender <matthewe2020@gmail.com>
2022-01-01 22:29:38 -0500

    Added prefix to .env

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,19 @@ impl EventHandler for Handler {}
 async fn main() {
     dotenv::dotenv().unwrap();
 
+    let token = env::var("DISCORD_TOKEN").expect("Failed to get bot token");
+    let prefix = env::var("BOT_PREFIX").expect("Failed to get discord bot prefix");
+
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!"))
+        .configure(|c| c.prefix(prefix)
         .group(&GENERAL_GROUP);
 
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
 
-    // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
