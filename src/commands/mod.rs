e00af5fc3c61ac345b3703c1bc12a950c0acb5d7
Matt Ehrnschwender <matthewe2020@gmail.com>
2022-01-04 14:04:54 -0500

    Readded cat

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -7,6 +7,7 @@ mod leet;
 mod help;
 mod linux;
 mod dog;
+mod cat;
 
 use joke::JOKE_COMMAND;
 use ping::PING_COMMAND;
@@ -15,7 +16,8 @@ use leet::LEET_COMMAND;
 use help::HELP_COMMAND;
 use linux::LINUX_COMMAND;
 use dog::DOG_COMMAND;
+use cat::CAT_COMMAND;
 
 #[group]
-#[commands(ping, joke, gnu, leet, help, linux, dog)]
+#[commands(ping, joke, gnu, leet, help, linux, dog, cat)]
 pub struct General;
