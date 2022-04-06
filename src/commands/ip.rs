695ee36e27826bb4e07144e760c0d8b3ce857865
Micah Wasmuth <micahwasmuth@gmail.com>
2022-04-05 21:30:19 -0400

    Fix link

--- src/commands/ip.rs
+++ src/commands/ip.rs
@@ -31,46 +31,40 @@ pub async fn ip(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 fn ip_info(client: &VtClient, ip: &str) -> String {
+    let link = format!("Link to analysis: <https://www.virustotal.com/gui/ip-address/{ip}>");
+
     match client.ip_info(ip) {
-        Ok(i) => {
-            let link = if let Some(l) = i.data.links {
-                format!("Link to analysis: {}", l.self_field.unwrap())
-            } else {
-                "Link to analysis not found. That's...not right.".to_owned()
-            };
+        Ok(i) => if let Some(a) = i.data.attributes {
+            if let Some(s) = a.last_analysis_stats {
+                let mut elements = Vec::new();
+                elements.push((s.harmless.unwrap(), "harmless"));
+                elements.push((s.malicious.unwrap(), "malicious"));
+                elements.push((s.suspicious.unwrap(), "suspicious"));
+                // elements.push((s.timeout.unwrap(), "timeout"));
+                // elements.push((s.undetected.unwrap(), "undetected"));
 
-            if let Some(a) = i.data.attributes {
-                if let Some(s) = a.last_analysis_stats {
-                    let mut elements = Vec::new();
-                    elements.push((s.harmless.unwrap(), "harmless"));
-                    elements.push((s.malicious.unwrap(), "malicious"));
-                    elements.push((s.suspicious.unwrap(), "suspicious"));
-                    // elements.push((s.timeout.unwrap(), "timeout"));
-                    // elements.push((s.undetected.unwrap(), "undetected"));
+                elements.sort_by(|a, b| b.0.cmp(&a.0));
 
-                    elements.sort_by(|a, b| b.0.cmp(&a.0));
-
-                    if elements[0].0 == 0 {
-                        "No data found for {ip}".to_owned()
-                    } else if elements[1].0 == 0 {
-                        format!("Most likely {} ({} hits). {}",
-                                elements[0].1,
-                                elements[0].0,
-                                link)
-                    } else {
-                        format!("Most likely {} ({} hits), but may be {} ({} hits). {}",
-                                elements[0].1,
-                                elements[0].0,
-                                elements[1].1,
-                                elements[1].0,
-                                link)
-                    }
+                if elements[0].0 == 0 {
+                    "No data found for {ip}".to_owned()
+                } else if elements[1].0 == 0 {
+                    format!("Most likely {} ({} hits). {}",
+                            elements[0].1,
+                            elements[0].0,
+                            link)
                 } else {
-                    "Analysis stats not found.".to_owned()
+                    format!("Most likely {} ({} hits), but may be {} ({} hits). {}",
+                            elements[0].1,
+                            elements[0].0,
+                            elements[1].1,
+                            elements[1].0,
+                            link)
                 }
             } else {
-                "Attributes not found.".to_owned()
+                "Analysis stats not found.".to_owned()
             }
+        } else {
+            "Attributes not found.".to_owned()
         }
         Err(e) => format!("Error: ```{}```", e.to_string()),
     }
