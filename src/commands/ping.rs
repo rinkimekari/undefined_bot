be4a3e2212c2eeec9768e4cf9fd74e86df5ebb71
Dylan McBean-Kyle <40377397+DylanMcBean@users.noreply.github.com>
2022-04-17 12:11:08 +0100

    Basic edit message test, Ping Command.

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -4,7 +4,11 @@ use serenity::model::channel::Message;
 
 #[command]
 pub async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+    let now = std::time::SystemTime::now(); // Get the current time
+    let message = msg.reply(ctx, "Pong!").await?; // Send initial message responce
+    let message_time = now.elapsed().unwrap(); // Get the time it took to send the message
+
+    message.edit(ctx, |m| m.content(format!("Pong! Took {}ms", message_time.as_millis()))).await?; // Edit the message to show the time it took to send it
 
     Ok(())
 }
