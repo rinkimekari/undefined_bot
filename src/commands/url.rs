e823bcaf79ff5624362486bea6e52e67e41802d5
Micah Wasmuth <micahwasmuth@gmail.com>
2022-05-01 15:00:36 -0400

    Listen to clippy

--- src/commands/url.rs
+++ src/commands/url.rs
@@ -1,13 +1,13 @@
 use serenity::client::Context;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::channel::Message;
-use vt3::VtClient;
 use url::Url as ParseUrl;
+use vt3::VtClient;
 
 #[command]
 pub async fn url(ctx: &Context, msg: &Message) -> CommandResult {
-    let api_key = std::env::var("VIRUSTOTAL_KEY").unwrap_or("".to_string());
-    if api_key == "" {
+    let api_key = std::env::var("VIRUSTOTAL_KEY").unwrap_or_else(|_| "".to_string());
+    if api_key.is_empty() {
         msg.reply(ctx, "No API key found \\:(").await?;
         return Ok(());
     }
@@ -16,7 +16,8 @@ pub async fn url(ctx: &Context, msg: &Message) -> CommandResult {
 
     let url = *args.get(1).unwrap_or(&"");
     if ParseUrl::parse(url).is_err() {
-        msg.reply(ctx, "Please provide a URL so I can actually do something.").await?;
+        msg.reply(ctx, "Please provide a URL so I can actually do something.")
+            .await?;
         return Ok(());
     }
 
@@ -30,40 +31,44 @@ pub async fn url(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 fn url_info(client: &VtClient, u: &str) -> String {
-    let link = format!("Link to analysis: Not available, but will eventually support analyses for domains.");
+    let link = "Link to analysis: Not available, but will eventually support analyses for domains."
+        .to_string();
 
     match client.url_info(u) {
-        Ok(i) => if let Some(d) = i.data {
-            if let Some(a) = d.attributes {
-                if let Some(s) = a.last_analysis_stats {
-                    let mut elements = Vec::new();
-                    elements.push((s.harmless.unwrap(), "harmless"));
-                    elements.push((s.malicious.unwrap(), "malicious"));
-                    elements.push((s.suspicious.unwrap(), "suspicious"));
+        Ok(i) => {
+            if let Some(d) = i.data {
+                if let Some(a) = d.attributes {
+                    if let Some(s) = a.last_analysis_stats {
+                        let mut elements = vec![
+                            (s.harmless.unwrap(), "harmless"),
+                            (s.malicious.unwrap(), "malicious"),
+                            (s.suspicious.unwrap(), "suspicious"),
+                        ];
 
-                    elements.sort_by(|a, b| b.0.cmp(&a.0));
+                        elements.sort_by(|a, b| b.0.cmp(&a.0));
 
-                    if elements[0].0 == 0 {
-                        "No data found for {u}".to_string()
-                    } else if elements[1].0 == 0 {
-                        format!("Most likely {} ({} hits). {link}",
-                                elements[0].1,
-                                elements[0].0)
+                        if elements[0].0 == 0 {
+                            "No data found for {u}".to_string()
+                        } else if elements[1].0 == 0 {
+                            format!(
+                                "Most likely {} ({} hits). {link}",
+                                elements[0].1, elements[0].0
+                            )
+                        } else {
+                            format!(
+                                "Most likely {} ({} hits), but may be {} ({} hits). {link}",
+                                elements[0].1, elements[0].0, elements[1].1, elements[1].0
+                            )
+                        }
                     } else {
-                        format!("Most likely {} ({} hits), but may be {} ({} hits). {link}",
-                                elements[0].1,
-                                elements[0].0,
-                                elements[1].1,
-                                elements[1].0)
+                        "Analysis stats not found.".to_string()
                     }
                 } else {
-                    "Analysis stats not found.".to_string()
+                    "Data not found.".to_string()
                 }
             } else {
-                "Data not found.".to_string()
+                "Attributes not found.".to_string()
             }
-        } else {
-            "Attributes not found.".to_string()
         }
         Err(e) => e.to_string(),
     }
