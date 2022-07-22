e416decbe413d46d32bca227c5348f10ff0f9956
Micah Wasmuth <micahwasmuth@gmail.com>
2022-07-21 21:49:55 -0400

    fix scottish better

--- src/commands/cunt.rs
+++ src/commands/cunt.rs
@@ -1,15 +1,16 @@
 use serenity::client::Context;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::channel::Message;
-use std::thread::sleep;
-use std::time::Duration;
 
 #[command]
 pub async fn cunt(ctx: &Context, msg: &Message) -> CommandResult {
-    for _ in 0..15 {
-        msg.reply(ctx, "<:scottish:964578746172600352>").await?;
-        sleep(Duration::from_secs(2));
-    }
+    let emoji = ctx
+        .http
+        .get_emoji(927022168963948595, 999846296745680906)
+        .await
+        .unwrap();
+
+    msg.reply(ctx, &emoji).await?;
 
     Ok(())
 }
